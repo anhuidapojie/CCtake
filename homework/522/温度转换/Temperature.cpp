d4a6912ea87fd28157333acb42185933d0a0c050
anhuidapojie <2977616761@qq.com>
2025-05-30 17:54:01 +0800

    温度转换

---
+++ homework/522/温度转换/Temperature.cpp
@@ -0,0 +1,4 @@
+#include "Temperature.h"
+void Temperature(int t){
+    cout << t+271.3 <<endl;
+}
---
+++ homework/522/温度转换/Temperature.h
@@ -0,0 +1,11 @@
+#ifndef _TEMPERATURE_
+#define _TEMPERATURE_
+
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief 转化为国际温度
+ */
+void Temperature(int t);
+#endif
