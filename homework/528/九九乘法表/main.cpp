27d692ee983656d81703c7d8ffe9a4cf664498cf
anhuidapojie <2977616761@qq.com>
2025-05-31 23:42:29 +0800

    九九乘法表

---
+++ homework/528/九九乘法表/main.cpp
@@ -0,0 +1,10 @@
+#include "mul.h"
+
+int main(int argc,char const * argv[])
+{
+    int n;
+    cout<<"请输入一个数"<<endl;
+    cin>>n;
+    mul(n);
+    return 0;
+}
---
+++ homework/528/九九乘法表/mul.h
@@ -0,0 +1,10 @@
+#ifndef _MUL_
+#define _MUL_
+#include <iostream>
+using namespace std;
+ 
+/**
+ * @brief 九九乘法表
+ */
+void mul(int n);
+#endif
