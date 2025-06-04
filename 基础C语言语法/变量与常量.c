3baf032ca650f19eb26297b2c53c4ac6eb4d40be
anhuidapojie <2977616761@qq.com>
2025-06-05 07:09:45 +0800

    变量与常量

--- 基础C语言语法/变量与常量.c
+++ 基础C语言语法/变量与常量.c
@@ -9,5 +9,10 @@ int main()
     b = 100;
     printf("%d\n",a);
     printf("%d\n",b);
+
+    //printf的第一个参数必须是字符串 （\数值，\a表示报警，\b表示退格，\f表示换页，\t水平制表,\v垂直制表，\r回车）转义字符
+    char a = 'a';
+    printf("包含的是字符串");
+    printf("字符常量由单引号包括%c",a);
     return 0;
 }
