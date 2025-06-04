48f9848aec1a1bf3994d41007000086cab642f10
anhuidapojie <2977616761@qq.com>
2025-06-05 06:45:10 +0800

    变量与常量

---
+++ 基础C语言语法/变量与常量.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #define 符号常量名 值
+int main()
+{
+    //变量声明后立即初始化
+    int a = 100;
+    //变量先声明，在赋值,
+    int b;
+    b = 100;
+    printf("%d\n",a);
+    printf("%d\n",b);
+    return 0;
+}
