f50c3cd4b5f1f34608ee69ea2c865065320940d8
anhuidapojie <2977616761@qq.com>
2025-06-05 23:34:32 +0800

    运算符

---
+++ 基础C语言语法/运算符.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2;
+    int b = 3;
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    printf("%d %d\n",a++,b++);
+    printf("%d %d\n",--a,--b);
+    return 0;
+}
