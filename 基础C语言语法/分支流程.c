6194dd761a638bd7b5b86ff2b774a5b5d94b00b9
anhuidapojie <2977616761@qq.com>
2025-06-06 18:24:52 +0800

    分支结构

---
+++ 基础C语言语法/分支流程.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("请输入一个数\n");
+    scanf("%d",&a);
+    if (a>=2 && a<=10)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    //条件运算符
+    char x,c;
+    if (x>=10)
+    {
+        c = 'Y';
+    }
+    else
+    {
+        x = 'N';
+    }
+    char x,c;
+    c = x >= 10?'Y':'N';
+    return 0;
+}
